be773794eb4b3b8c6397d70dab73b5f4f587614f
CoderContentGit <codercontent@hotmail.com>
2025-08-28 22:46:10 +0100

    posts pagination

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-import express from 'express';
+import express from "express";
 import postsRouter from "./routes/posts.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
-
 app.use("/posts", postsRouter);
 
-app.use('*', (req, res) => {
-  res.status(404).json({ error: 'Route not found' });
-});
-
 app.listen(PORT, () => {
-  console.log(`🚀 Express server is running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
-
-module.exports = app;
